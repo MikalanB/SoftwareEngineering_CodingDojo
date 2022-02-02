f66598de4cb605d66e8eac23768f22ee5ce58b81
Mikalan Ballentine <mikalanballentine@gmail.com>
2022-02-02 11:04:01 -0500

    master

---
+++ WebFun2022/LikesAssignment/script.js
@@ -0,0 +1,22 @@
+// var count = document.querySelector(".count-likes").innerHTML; 
+
+function hitClick() {
+    count = document.querySelector(".count-likes1").innerHTML;
+    count++;
+    console.log(count);
+    document.querySelector(".count-likes1").innerHTML = count;
+}
+
+function hitClick2() {
+    count = document.querySelector(".count-likes2").innerHTML;
+    count++;
+    console.log(count);
+    document.querySelector(".count-likes2").innerHTML = count;
+}
+
+function hitClick3() {
+    count = document.querySelector(".count-likes3").innerHTML;
+    count++;
+    console.log(count);
+    document.querySelector(".count-likes3").innerHTML = count;
+}
