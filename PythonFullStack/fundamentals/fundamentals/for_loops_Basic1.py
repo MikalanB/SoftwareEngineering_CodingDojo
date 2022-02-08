2cd3f1b6f312fe1a4a9f4398d57d94d2571661ff
Mikalan Ballentine <mikalanballentine@gmail.com>
2022-02-07 21:49:53 -0500

    completed master

--- PythonFullStack/fundamentals/fundamentals/for_loops_Basic1.py
+++ PythonFullStack/fundamentals/fundamentals/for_loops_Basic1.py
@@ -29,3 +29,9 @@ for a in range(2018, 0, -4):
     print(a)
 
 # Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+def flexible_counter (lowNum, highNum, mult):
+    for x in range(lowNum, highNum+1):
+        if (x % mult == 0):
+            print(x)
+
+print(flexible_counter(2,9,3))
