4e4c0cb347546768eccddc1162c6be5bc38ccb15
MikalanB <77364785+MikalanB@users.noreply.github.com>
2022-02-19 01:40:49 -0500

    updated CRUD user master

--- PythonFullStack/flask_mysql/crud/users_fullstack/server.py
+++ PythonFullStack/flask_mysql/crud/users_fullstack/server.py
@@ -2,15 +2,19 @@ from flask import Flask, redirect, request, render_template
 from user import User
 app = Flask(__name__)
 
+# home page route
 @app.route('/')
+@app.route('/users')
 def index():
     users = User.all_users()
     return render_template('index.html', users = users)
 
+# create user page 
 @app.route('/create_user')
 def create_user():
     return render_template('create.html')
 
+# add user route to retrieve form info
 @app.route('/add_user', methods = ['POST'])
 def add_user():
     data = {
@@ -19,7 +23,52 @@ def add_user():
         "email": request.form['email']
     }
     User.add_user(data)
-    return redirect ('/')
+    return redirect ('/users')
+
+# action 1 from home page - show user info
+@app.route('/show/<int:user_id>')
+def show_user(user_id):
+    data = {
+        "id" : user_id
+    }
+    user_info = User.show_user(data)
+    return render_template('show_user.html', user_info=user_info)
+
+# action 2- edit user info
+@app.route('/edit/<int:user_id>')
+def edit_user(user_id):
+    data = {
+        "id" : user_id
+    }
+    user = User.show_user(data)
+    return render_template('edit_user.html', user=user)
+
+@app.route('/update/<int:user_id>', methods=['post'])
+def update_user(user_id):
+    data = {
+        "user_id" : user_id,
+        "first_name" : request.form['first_name'],
+        "last_name" : request.form['last_name'],
+        "email" : request.form['email']
+    }
+    edit_user = User.edit_user(data)
+    return redirect('/users')
+
+
+@app.route('/delete/<int:user_id>')
+def delete_user(user_id):
+    data = {
+        "user_id" : user_id
+    }
+    delete_user = User.delete_user(data)
+    return redirect('/users')
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
