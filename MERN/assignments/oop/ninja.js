edb9848f51b189311bbf034c2495560e412a3ea8
MikalanB <77364785+MikalanB@users.noreply.github.com>
2022-03-08 15:18:35 -0500

    master

---
+++ MERN/assignments/oop/ninja.js
@@ -0,0 +1,28 @@
+class Ninja {
+    constructor (name, health, speed = 3, strength =3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName () {
+        console.log(`${this.name} is my name`);
+    }
+
+    showStats () {
+        console.log(`${this.name}, ${this.health}, ${this.speed}, ${this.strength}`);
+    }
+
+    drinkSake (ninja) {
+        ninja.health += 10;
+        console.log(`Health is now +10 to equal ${ninja.health}`)
+    }
+}
+
+const mikalan = new Ninja('Mikalan', 80, 5);
+//console.log(mikalan)
+mikalan.sayName();
+mikalan.showStats();
+mikalan.drinkSake(mikalan);
+mikalan.showStats();
