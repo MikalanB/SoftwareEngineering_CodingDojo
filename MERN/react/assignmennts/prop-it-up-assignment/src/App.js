f2ddb6de28f1e38bcbdc18b89c1f887cf1d76add
MikalanB <77364785+MikalanB@users.noreply.github.com>
2022-03-09 16:12:12 -0500

    master

---
+++ MERN/react/assignmennts/prop-it-up-assignment/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PersonCard from './components/PersonCard'
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard firstName = 'Jane' lastName = "Doe" age = '45' hairColor = "Black" ></PersonCard>
+      <PersonCard firstName = 'John' lastName = "Smith" age = '88' hairColor = "Brown" ></PersonCard>
+      <PersonCard firstName = 'Millard' lastName = "Filmore" age = '50' hairColor = "Brown" ></PersonCard>
+      <PersonCard firstName = 'Maria' lastName = "Smith" age = '62' hairColor = "Brown" ></PersonCard>
+
+    </div>
+  );
+}
+
+export default App;
