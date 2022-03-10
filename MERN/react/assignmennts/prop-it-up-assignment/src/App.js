97552b0e4c433a818328d055f3da883250475b68
MikalanB <77364785+MikalanB@users.noreply.github.com>
2022-03-09 22:38:51 -0500

    master

--- MERN/react/assignmennts/prop-it-up-assignment/src/App.js
+++ MERN/react/assignmennts/prop-it-up-assignment/src/App.js
@@ -5,10 +5,10 @@ import PersonCard from './components/PersonCard'
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName = 'Jane' lastName = "Doe" age = '45' hairColor = "Black" ></PersonCard>
-      <PersonCard firstName = 'John' lastName = "Smith" age = '88' hairColor = "Brown" ></PersonCard>
-      <PersonCard firstName = 'Millard' lastName = "Filmore" age = '50' hairColor = "Brown" ></PersonCard>
-      <PersonCard firstName = 'Maria' lastName = "Smith" age = '62' hairColor = "Brown" ></PersonCard>
+      <PersonCard firstName = 'Jane' lastName = "Doe" age = {45} hairColor = "Black" ></PersonCard>
+      <PersonCard firstName = 'John' lastName = "Smith" age = {88} hairColor = "Brown" ></PersonCard>
+      <PersonCard firstName = 'Millard' lastName = "Filmore" age = {50} hairColor = "Brown" ></PersonCard>
+      <PersonCard firstName = 'Maria' lastName = "Smith" age = {62} hairColor = "Brown" ></PersonCard>
 
     </div>
   );
--- MERN/react/assignmennts/prop-it-up-assignment/src/components/PersonCard.js
+++ MERN/react/assignmennts/prop-it-up-assignment/src/components/PersonCard.js
@@ -3,14 +3,26 @@ import React, {Component} from 'react';
 class PersonCard extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            age: this.props.age
+        }
     }
+
+    increaseAge = () => {
+        this.setState({ 
+            age: this.state.age + 1
+        })
+    }
+
     render() {
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
         return(
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.increaseAge}>Birthday Button for {firstName} {lastName}</button>
+                <br></br>
             </div>
         )
     }
