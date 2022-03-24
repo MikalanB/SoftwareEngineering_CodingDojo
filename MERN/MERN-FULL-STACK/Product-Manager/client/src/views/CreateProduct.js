86e292fa15536f0510124c2f75bb882d6083f414
MikalanB <77364785+MikalanB@users.noreply.github.com>
2022-03-24 14:25:15 -0400

    master

--- MERN/MERN-FULL-STACK/Product-Manager/client/src/views/CreateProduct.js
+++ MERN/MERN-FULL-STACK/Product-Manager/client/src/views/CreateProduct.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import {useHistory} from 'react-router-dom';
+import Form from '../components/Form';
 
 const CreateProduct = (props) => {
+
     const [form, setForm] = useState({
         title: "",
         price: 0,
@@ -21,7 +23,7 @@ const CreateProduct = (props) => {
     const onSubmitHandler = (event) => {
         event.preventDefault();
         
-        console.log("You working?")
+        console.log("Successful")
 
         axios.post("http://localhost:8000/api/products/create", form)
             .then(res => {
@@ -33,27 +35,8 @@ const CreateProduct = (props) => {
 
     return (
         <div>
-            <h1>Product Manager</h1>
-            <div className="d-flex flex-column w-50 justify-content-center mx-auto">
-                <form onSubmit={onSubmitHandler} className="row g-3 needs-validation">
-                    <div className="form-floating mb-3">
-                        <input type="text" className="form-control" id="floatingInput" placeholder="Title of Product" name="title" onChange={onChangeHandler} />
-                        <label htmlFor="floatingInput">Title</label>
-                        {form.title.length === 0 ? "" : form.title.length < 3 ? <span className="alert-danger">Title needs at least 3 characters!</span> : ""}
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input type="text" className="form-control" id="floatingInput" placeholder="Price of Product" name="price" onChange={onChangeHandler} />
-                        <label htmlFor="floatingInput">Price</label>
-                        {form.price === 0 ? "" : form.price < 1 ? <span className="alert-danger">Price needs to be at least $1!</span> : ""}
-                    </div>
-                    <div className="form-floating mb-3">
-                        <textarea type="text" className="form-control" id="floatingInput" placeholder="name@example.com" style={{height: "150px"}} name="description" onChange={onChangeHandler}></textarea>
-                        <label htmlFor="floatingInput">Description</label>
-                        {form.description.length === 0 ? "" : form.description.length < 10 ? <span className="alert-danger">Description needs at least 10 characters!</span> : ""}
-                    </div>
-                    <input type="submit" className="btn btn-primary btn-lg " value="Create"/>
-                </form>
-            </div>
+            <Form form={form} onSubmitProp={onSubmitHandler} onChangeHandlerProp={onChangeHandler} />
+            {/* <CreateAndUpdate initialTitle="" initialPrice="" initialDescription="" onSubmitProp={createProduct}/> */}
         </div>
     )
 }
--- MERN/MERN-FULL-STACK/Product-Manager/client/src/views/Update.js
+++ MERN/MERN-FULL-STACK/Product-Manager/client/src/views/Update.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import {useParams, useHistory} from 'react-router-dom';
 import {useState, useEffect} from 'react';
+import Form from '../components/Form';
 
 const Update = (props) => {
     const {_id} = useParams();
@@ -47,29 +48,7 @@ const Update = (props) => {
     }
 
     return (
-        <div>
-            <h1>Edit Product</h1>
-            <div className="d-flex flex-column w-50 justify-content-center mx-auto">
-                <form onSubmit={onSubmitHandler} className="row g-3 needs-validation">
-                    <div className="form-floating mb-3">
-                        <input type="text" className="form-control" id="floatingInput" placeholder="Title of Product" name="title" onChange={onChangeHandler} value={form.title} />
-                        <label htmlFor="floatingInput">Title</label>
-                        {form.title.length === 0 ? "" : form.title.length < 3 ? <span className="alert-danger">Title needs at least 3 characters!</span> : ""}
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input type="text" className="form-control" id="floatingInput" placeholder="Price of Product" name="price" onChange={onChangeHandler} value={form.price} />
-                        <label htmlFor="floatingInput">Price</label>
-                        {form.price === 0 ? "" : form.price < 1 ? <span className="alert-danger">Price needs to be at least $1!</span> : ""}
-                    </div>
-                    <div className="form-floating mb-3">
-                        <textarea type="text" className="form-control" id="floatingInput" placeholder="name@example.com" style={{height: "150px"}} name="description" onChange={onChangeHandler} value={form.description} ></textarea>
-                        <label htmlFor="floatingInput">Description</label>
-                        {form.description.length === 0 ? "" : form.description.length < 10 ? <span className="alert-danger">Description needs at least 10 characters!</span> : ""}
-                    </div>
-                    <input type="submit" className="btn btn-primary btn-lg " value="Update"/>
-                </form>
-            </div>
-        </div>
+        <Form form={form} onSubmitProp={onSubmitHandler} onChangeHandlerProp={onChangeHandler}/>
     )
 }
 
