e1361414d68d39f1547d8d953a8c206de6e9efae
MikalanB <77364785+MikalanB@users.noreply.github.com>
2022-04-10 01:09:04 -0400

    master

---
+++ JAVA/assignments/ordersAndItems/Item.java
@@ -0,0 +1,14 @@
+//package assignments.ordersAndItems;
+
+public class Item {
+    
+    String name;
+    double price;
+
+    // public String nameInput(String name) {
+    //     return name;
+    // }
+    // public double priceInput(double price) {
+    //     return price;
+    // }
+}
